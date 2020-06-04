32a1bf346b222c6c1efe15e842cc0b1357020946
Marko <markogajiin@gmail.com>
2020-06-04 20:41:34 +0200

    fix

--- app/src/main/java/rs/raf/projekat2/marko_gajin_RM8517/data/models/ui/Employee.kt
+++
@@ -1,8 +0,0 @@
-package rs.raf.projekat2.marko_gajin_RM8517.data.models.ui
-
-data class Employee(
-    val id: String,
-    val name: String,
-    val salary: String,
-    val age: String
-)
--- app/src/main/java/rs/raf/projekat2/marko_gajin_RM8517/data/repositories/ScheduleRepository.kt
+++ app/src/main/java/rs/raf/projekat2/marko_gajin_RM8517/data/repositories/ScheduleRepository.kt
@@ -6,7 +6,5 @@ import rs.raf.projekat2.marko_gajin_RM8517.data.models.ui.Lecture
 interface ScheduleRepository {
 
     fun getSchedule(): Observable<List<Lecture>>
-
-//    fun getLecture(name: String): Observable<Lecture>
-
+    
 }
