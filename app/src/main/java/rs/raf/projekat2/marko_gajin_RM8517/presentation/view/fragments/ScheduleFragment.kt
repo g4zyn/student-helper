30647e1782c348d44125f844b84fbcdd07d9ffee
Marko <markogajiin@gmail.com>
2020-06-06 15:32:27 +0200

    schedule fragment layout

--- app/src/main/java/rs/raf/projekat2/marko_gajin_RM8517/presentation/view/fragments/ScheduleFragment.kt
+++ app/src/main/java/rs/raf/projekat2/marko_gajin_RM8517/presentation/view/fragments/ScheduleFragment.kt
@@ -1,8 +1,9 @@
 package rs.raf.projekat2.marko_gajin_RM8517.presentation.view.fragments
 
 import androidx.fragment.app.Fragment
+import rs.raf.projekat2.marko_gajin_RM8517.R
 
-class ScheduleFragment : Fragment() {
+class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
 
 
 
