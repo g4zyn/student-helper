f7c6fbd28bd204522b59c7a0c00afed7ba667c6e
aleksandar234 <aleksandar.stojanovic19982@gmail.com>
2020-06-07 22:38:00 +0200

    'Test'

--- app/src/main/java/rs/raf/projekat2/marko_gajin_RM8517/data/datasources/local/Database.kt
+++ app/src/main/java/rs/raf/projekat2/marko_gajin_RM8517/data/datasources/local/Database.kt
@@ -6,7 +6,7 @@ import rs.raf.projekat2.marko_gajin_RM8517.data.models.LectureEntity
 import rs.raf.projekat2.marko_gajin_RM8517.data.models.NoteEntity
 
 @Database(
-    entities = [LectureEntity::class, NoteEntity::class],
+    entities = [NoteEntity::class,LectureEntity::class],
     version = 1,
     exportSchema = false
 )
